4954a886ba89630671708f29a267061a58db32ae
null <60427892+null8626@users.noreply.github.com>
2022-10-22 11:22:23 +0700

    refactor(getMS): assign to string variable later

--- src/utility/getMS.ts
+++ src/utility/getMS.ts
@@ -7,38 +7,32 @@ export default (time: number) => {
         value = Math.floor(time / year);
         time -= year * value;
         type = "year"
-        string = `${value} year${value !== 1 ? "s" : ""} `
     } else if (time > week) {
         value = Math.floor(time / week);
         time -= week * value;
         type = "week"
-        string = `${value} week${value !== 1 ? "s" : ""} `
     } else if (time > day) {
         value = Math.floor(time / day);
         time -= day * value;
         type = "day"
-        string = `${value} day${value !== 1 ? "s" : ""} `
     } else if (time > hour) {
         value = Math.floor(time / hour);
         time -= hour * value;
         type = "hour"
-        string = `${value} hour${value !== 1 ? "s" : ""} `
     } else if (time > minute) {
         value = Math.floor(time / minute);
         time -= minute * value;
         type = "minute"
-        string = `${value} minute${value !== 1 ? "s" : ""} `
     } else if (time > second) {
         value = Math.floor(time / second);
         time -= second * value;
         type = "second"
-        string = `${value} second${value !== 1 ? "s" : ""} `
     } else {
         value = time;
         time = 0;
         type = "millisecond"
-        string = `${value} millisecond${value !== 1 ? "s" : ""} `
     }
-
+    
+    string = `${value} ${type}${value !== 1 ? "s" : ""} `;
     return { time, value, string, type };
 }
