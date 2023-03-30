0100249301fdec6fbdfbd07292abec7286653976
WilcoSp <wilcosp@hotmail.nl>
2023-03-30 17:15:26 +0200

    updated some things

--- src/utility/getType.ts
+++ src/utility/getType.ts
@@ -1,9 +1,9 @@
 const second = 1000,
-  minute = second * 60,
-  hour = minute * 60,
-  day = hour * 24,
-  week = day * 7,
-  year = day * 365.25;
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24,
+    week = day * 7,
+    year = day * 365.25;
 
 // regexes
 const YEAR_REGEX = /^y((rs?)|(ear(s|\(s\))?))?$/;
@@ -14,13 +14,14 @@ const MINUTE_REGEX = /^m((ins?)|(inute(s|\(s\))?))?$/;
 const SECOND_REGEX = /^s((ecs?)|(econd(s|\(s\))?))?$/;
 const MILLISECOND_REGEX = /^(ms|(msecs?)|(millisecond(s|\(s\))?))?$/;
 
-export default (string: string) => {
-  if (YEAR_REGEX.test(string)) return year;
-  else if (WEEK_REGEX.test(string)) return week;
-  else if (DAY_REGEX.test(string)) return day;
-  else if (HOUR_REGEX.test(string)) return hour;
-  else if (MINUTE_REGEX.test(string)) return minute;
-  else if (SECOND_REGEX.test(string)) return second;
-  else if (MILLISECOND_REGEX.test(string)) return 1;
-  else return undefined;
-};
+export default function (value: string): number | null {
+    value = value.trim();
+    if (YEAR_REGEX.test(value)) return year;
+    else if (WEEK_REGEX.test(value)) return week;
+    else if (DAY_REGEX.test(value)) return day;
+    else if (HOUR_REGEX.test(value)) return hour;
+    else if (MINUTE_REGEX.test(value)) return minute;
+    else if (SECOND_REGEX.test(value)) return second;
+    else if (MILLISECOND_REGEX.test(value)) return 1;
+    else return null;
+}
--- src/utility/toMS.ts
+++ src/utility/toMS.ts
@@ -1,8 +1,22 @@
 import convert from './getType.js';
 
+/**
+ * Just a Number
+ * @param value value you want to check if it's a number
+ * @returns whether the value is a number
+ */
 function JaN(value: string): value is `${number}` {
     //@ts-ignore
     return value == parseFloat(value);
+}
+
+/**
+ * makes a number absolute
+ * @param value a number or bigint
+ * @returns absolute value
+ */
+function abs(value: number) {
+    return value < 0n ? -value : value;
 }
 
 /**
@@ -10,41 +24,86 @@ function JaN(value: string): value is `${number}` {
  * @param {String} value The human readable time string.
  * @returns {Number} The time in milliseconds.
  */
-
-// 1 hour 10min
-export default function (value: string): number | undefined {
+export default function (value: string): number | null {
+    value = value.trim().toLowerCase();
     if (JaN(value)) {
         return parseFloat(value);
     }
-    const strings = value.toLowerCase().trim().split(/ +/g);
-    let number = 0,
-        fails = 0,
-        trials = 0;
+    const values = value.split(/ +/g);
+    let ms = 0;
+    // fails = 0,
+    // trials = 0;
 
-    strings.forEach((v, i) => {
-        trials++;
-
-        // Value is a number
-        if (JaN(v)) {
-            let t = Math.abs(parseFloat(v));
-            if (JaN(strings[i + 1]) || !strings[i + 1]) {
-                number += t;
-                fails--;
-            } // Next value is also a number
-            else {
-                let value = convert(strings[i + 1].trim()?.toLowerCase());
-                if (value) number += value * t;
-                else fails++;
+    ms: for (let [i, v] of values.entries()) {
+        v = v.trim();
+        if (JaN(v.trim())) {
+            const n = abs(parseFloat(v)),
+                next = values[i + 1]?.trim();
+            if (JaN(next) || !next) {
+                // second value is a number/null, so current value is ms
+                ms += n;
+                continue ms;
             }
-        } else {
-            let no = parseFloat(v),
-                str = v.substring(no.toString()?.length)?.trim()?.toLowerCase();
-            let value = convert(str);
-
-            if (value && no) number += no * value;
-            else fails++;
+            // second value is a string
+            const time = convert(next);
+            if (!time) {
+                return null;
+            }
+            ms += n * time;
+            continue ms;
         }
-    });
+        const nr = parseFloat(v);
+        if (Number.isNaN(nr)) {
+            const previous = values[i - 1];
+            if (JaN(previous)) {
+                const c = convert(v);
+                if (c) {
+                    continue ms; // we've already done this one;
+                }
+                return null; // unmatched kind
+            }
+            const c = convert(v);
+            if (!c) {
+                return null;
+            }
+            ms += c;
+            continue ms;
+        }
 
-    return fails === strings.length ? undefined : number;
+        const c = convert(v.substring(nr.toString().length));
+        if (!c) {
+            return null;
+        }
+        ms += nr * c;
+        continue ms;
+    }
+
+    return ms;
+
+    // strings.forEach((v, i) => {
+    //     trials++;
+
+    //     // Value is a number
+    //     if (JaN(v)) {
+    //         let t = Math.abs(parseFloat(v));
+    //         if (JaN(strings[i + 1]) || !strings[i + 1]) {
+    //             number += t;
+    //             fails--;
+    //         } // Next value is also a number
+    //         else {
+    //             let value = convert(strings[i + 1].trim()?.toLowerCase());
+    //             if (value) number += value * t;
+    //             else fails++;
+    //         }
+    //     } else {
+    //         let no = parseFloat(v),
+    //             str = v.substring(no.toString()?.length)?.trim()?.toLowerCase();
+    //         let value = convert(str);
+
+    //         if (value && no) number += no * value;
+    //         else fails++;
+    //     }
+    // });
+
+    // return fails === values.length ? undefined : ms;
 }
